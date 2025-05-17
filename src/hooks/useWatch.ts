6f515a7412094026726851a5cd67f990830258a1
Shervin Ghajar <ssghajar.work@gmail.com>
2025-05-17 14:54:22 +0330

    build: 1.1.2

--- src/hooks/useWatch.ts
+++ src/hooks/useWatch.ts
@@ -1,40 +1,25 @@
-import type { Context } from 'react';
+import type { Context } from "react";
+import { useContext } from "react";
+import { Control, FieldValues } from "react-hook-form";
+import { DeepPartialSkipArrayKey, FieldPath, FieldPathValue, FieldPathValues, UseFormReturn, useWatch as useRHFWatch } from "react-hook-form";
 
 /* eslint-disable react-hooks/rules-of-hooks */
-import { useContext } from 'react';
-import { FieldValues } from 'react-hook-form';
-import {
-  DeepPartialSkipArrayKey,
-  FieldPath,
-  FieldPathValue,
-  FieldPathValues,
-  UseFormReturn,
-  useWatch as useRHFWatch,
-} from 'react-hook-form';
 
-const getUseWatch = <TFieldValues extends FieldValues>(
-  Context: Context<UseFormReturn<TFieldValues, object>>,
-) => {
-  function useWatch(props: {
-    defaultValue?: DeepPartialSkipArrayKey<TFieldValues>;
-    disabled?: boolean;
-    exact?: boolean;
-  }): DeepPartialSkipArrayKey<TFieldValues>;
+const getUseWatch = <TFieldValues extends FieldValues>(Context: Context<UseFormReturn<TFieldValues, object>>) => {
+  function useWatch(props: { defaultValue?: DeepPartialSkipArrayKey<TFieldValues>; disabled?: boolean; exact?: boolean }): TFieldValues;
   function useWatch<TFieldName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>>(props: {
     name: TFieldName;
     defaultValue?: FieldPathValue<TFieldValues, TFieldName>;
     disabled?: boolean;
     exact?: boolean;
   }): FieldPathValue<TFieldValues, TFieldName>;
-  function useWatch<
-    TFieldNames extends Array<FieldPath<TFieldValues>> = Array<FieldPath<TFieldValues>>,
-  >(props: {
+  function useWatch<TFieldNames extends Array<FieldPath<TFieldValues>> = Array<FieldPath<TFieldValues>>>(props: {
     name: readonly [...TFieldNames];
     defaultValue?: DeepPartialSkipArrayKey<TFieldValues>;
     disabled?: boolean;
     exact?: boolean;
   }): FieldPathValues<TFieldValues, TFieldNames>;
-  function useWatch(): DeepPartialSkipArrayKey<TFieldValues>;
+  function useWatch(): TFieldValues;
 
   function useWatch(props?: {
     exact?: boolean;
@@ -43,11 +28,12 @@ const getUseWatch = <TFieldValues extends FieldValues>(
     defaultValue?: unknown;
   }) {
     const context = useContext(Context);
+    const control = context.control as Control<TFieldValues>;
     const { defaultValue, name, disabled, exact } = props || {};
 
     if (name) {
       return useRHFWatch<TFieldValues>({
-        control: context.control,
+        control,
         defaultValue: defaultValue as DeepPartialSkipArrayKey<TFieldValues> | undefined,
         name: name as any,
         disabled,
@@ -55,12 +41,12 @@ const getUseWatch = <TFieldValues extends FieldValues>(
       });
     } else if (props) {
       return useRHFWatch<TFieldValues>({
-        control: context.control,
+        control,
         defaultValue: defaultValue as DeepPartialSkipArrayKey<TFieldValues> | undefined,
         disabled,
         exact,
       });
-    } else return useRHFWatch<TFieldValues>();
+    } else return useRHFWatch<TFieldValues>() as TFieldValues;
   }
   return useWatch;
 };
