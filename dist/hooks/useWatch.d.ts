b2a84eff6521605b78f5350e6b047dc8f60c8905
Shervin Ghajar <ssghajar.work@gmail.com>
2025-05-17 15:00:18 +0330

    build:  v1.1.2

--- dist/hooks/useWatch.d.ts
+++ dist/hooks/useWatch.d.ts
@@ -1,12 +1,12 @@
-import type { Context } from 'react';
-import { FieldValues } from 'react-hook-form';
-import { DeepPartialSkipArrayKey, FieldPathValue, FieldPathValues, UseFormReturn } from 'react-hook-form';
+import type { Context } from "react";
+import { FieldValues } from "react-hook-form";
+import { DeepPartialSkipArrayKey, FieldPathValue, FieldPathValues, UseFormReturn } from "react-hook-form";
 declare const getUseWatch: <TFieldValues extends FieldValues>(Context: Context<UseFormReturn<TFieldValues, object>>) => {
     (props: {
         defaultValue?: DeepPartialSkipArrayKey<TFieldValues> | undefined;
         disabled?: boolean | undefined;
         exact?: boolean | undefined;
-    }): DeepPartialSkipArrayKey<TFieldValues>;
+    }): TFieldValues;
     <TFieldName extends import("react-hook-form").Path<TFieldValues> = import("react-hook-form").Path<TFieldValues>>(props: {
         name: TFieldName;
         defaultValue?: import("react-hook-form").PathValue<TFieldValues, TFieldName> | undefined;
@@ -19,7 +19,7 @@ declare const getUseWatch: <TFieldValues extends FieldValues>(Context: Context<U
         disabled?: boolean | undefined;
         exact?: boolean | undefined;
     }): { [K in keyof TFieldNames]: import("react-hook-form").PathValue<TFieldValues, TFieldNames[K] & import("react-hook-form").Path<TFieldValues>>; };
-    (): DeepPartialSkipArrayKey<TFieldValues>;
+    (): TFieldValues;
 };
 export { getUseWatch };
 //# sourceMappingURL=useWatch.d.ts.map
